41cd1ccc75e1c154d4a12f884d30819d172493b5
mcmicko <mcmzrc@gmail.com>
2022-05-22 19:32:02 +0200

    fixing section 1

--- script.js
+++ script.js
@@ -14,18 +14,14 @@ function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.remove("close");
     menu.classList.add("hide");
-    // menuNav.classList.add("show");
     bgDiv.classList.remove("bg-gray");
     document.body.style.overflow = "visible";
-    // navItems.forEach((item) => item.classList.add("show"));
 
     showMenu = true;
   } else {
     menuBtn.classList.add("close");
     menu.classList.remove("hide");
-    // menuNav.classList.remove("show");
     bgDiv.classList.add("bg-gray");
-    // navItems.forEach((item) => item.classList.remove("show"));
     document.body.style.overflow = "hidden";
 
     showMenu = false;
