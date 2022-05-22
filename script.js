69cdb23c6e1a30712f66fb33f2961ba2a21e3f10
mcmicko <mcmzrc@gmail.com>
2022-05-22 17:10:40 +0200

    Fix bugs

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const header = document.querySelector("header");
+const bgDiv = document.querySelector(".bg");
 const navItems = document.querySelectorAll(".nav-item");
 
 let showMenu = false;
@@ -13,17 +14,19 @@ function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.remove("close");
     menu.classList.add("hide");
-    menuNav.classList.add("show");
-    header.classList.remove("bg-gray");
-    navItems.forEach((item) => item.classList.add("show"));
+    // menuNav.classList.add("show");
+    bgDiv.classList.remove("bg-gray");
+    document.body.style.overflow = "visible";
+    // navItems.forEach((item) => item.classList.add("show"));
 
     showMenu = true;
   } else {
     menuBtn.classList.add("close");
     menu.classList.remove("hide");
-    menuNav.classList.remove("show");
-    header.classList.add("bg-gray");
-    navItems.forEach((item) => item.classList.remove("show"));
+    // menuNav.classList.remove("show");
+    bgDiv.classList.add("bg-gray");
+    // navItems.forEach((item) => item.classList.remove("show"));
+    document.body.style.overflow = "hidden";
 
     showMenu = false;
   }
